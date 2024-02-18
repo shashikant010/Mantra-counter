907877b7086ee96b392566539acd0e75e89abcbd
shashikant010 <shashikantyadav0503@gmail.com>
2024-02-18 17:40:09 +0530

    added shankh to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import favicon from '../favicon.ico'
+import shankh from '../images/shankh.png'
+import shankhsound from '../music/shankh.mp3'
 
 export default function Navbar() {
+  const handleshankhclick=()=>{
+    document.getElementById('shankh').play();
+  }
   return (
     <div className='Navbar'>
      <h1><img src={favicon} alt="img" height='30px' />&nbsp;SKY-ASTHA &nbsp;<img src={favicon} alt="img" height='30px' /> </h1>
+     <div className="shankh" onClick={handleshankhclick}>
+      <img src={shankh} alt="" srcset="" />
+     </div>
+     <audio src={shankhsound} id='shankh'></audio>
     </div>
   )
 }
