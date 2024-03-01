7a5c0eba2c06cbdb453226193c4ec96239debca2
shashikant010 <shashikantyadav0503@gmail.com>
2024-03-01 17:45:12 +0530

    fixed a bug

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -3,11 +3,11 @@ import arti from '../images/aarti.png'
 import {Link} from 'react-router-dom'
 export default function Boxes(props) {
   return (
-    
-      <div className='boxes'>
+    <Link to={`/${props.title}`}className='boxes'>
+      
         <img src={arti} alt="" height={100}/>
-<Link to={`/${props.title}`}><h2>{props.title}</h2></Link>
-      </div>
-    
+<h2>{props.title}</h2>
+      
+    </Link>
   )
 }
